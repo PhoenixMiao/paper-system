bb73cad9e1eacd034cee1913a8494c4a0aa27faa
PhoenixMiao <2462660882@qq.com>
2022-06-02 21:05:11 +0800

    添加论文的相关操作

--- src/main/java/com/phoenix/paper/mapper/ResearchDirectionMapper.java
+++ src/main/java/com/phoenix/paper/mapper/ResearchDirectionMapper.java
@@ -3,14 +3,13 @@ package com.phoenix.paper.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.phoenix.paper.dto.BriefNode;
 import com.phoenix.paper.entity.ResearchDirection;
-import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
 public interface ResearchDirectionMapper extends BaseMapper<ResearchDirection> {
-    @Select("SELECT id,name,is_leaf FROM research_direction WHERE father_id = #{father_id} AND LEN(delete_time) = 0 AND delete_time IS NULL;")
+    @Select("SELECT id,name,is_leaf FROM research_direction WHERE father_id = #{father_id} AND delete_time IS NULL;")
     List<BriefNode> getSons(@Param("father_id")Long father_id);
 
     @Select("SELECT id FROM research_direction WHERE root_id = #{root_id} AND path LIKE #{path} AND LEN(delete_time) = 0 AND delete_time IS NULL;")
