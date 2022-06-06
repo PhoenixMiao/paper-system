0ebd0a42b4cb426771a9c19da73e3a38b56be33f
ZhuYan <2356773295@qq.com>
2022-06-06 14:22:47 +0800

    获取论文笔记数据 功能实现

--- src/main/java/com/phoenix/paper/mapper/NoteMapper.java
+++ src/main/java/com/phoenix/paper/mapper/NoteMapper.java
@@ -1,6 +1,7 @@
 package com.phoenix.paper.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.phoenix.paper.dto.PaperAndNoteData;
 import com.phoenix.paper.entity.Note;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
@@ -19,4 +20,7 @@ public interface NoteMapper extends BaseMapper<Note> {
 
     @Select("SELECT * FROM note WHERE delete_time IS NULL")
     List<Note> getNoteList();
+
+    @Select("SELECT direction_id as direction,count(*) as number FROM note LEFT JOIN paper_direction ON note.paper_id=paper_direction.paper_id  where author_id=#{userId} AND TO_DAYS(NOW())-TO_DAYS(upload_time)<=#{period} group by direction_id ")
+    List<PaperAndNoteData> getNoteData(@Param("user_id")Long userId, @Param("period")Integer period);
 }
