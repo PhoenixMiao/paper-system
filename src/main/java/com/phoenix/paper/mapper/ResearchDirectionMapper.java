3d9ea7eb1e1a44d798905673822ca3a4abbf32fd
PhoenixMiao <2462660882@qq.com>
2022-06-09 21:14:38 +0800

    权值搜索

--- src/main/java/com/phoenix/paper/mapper/ResearchDirectionMapper.java
+++ src/main/java/com/phoenix/paper/mapper/ResearchDirectionMapper.java
@@ -12,7 +12,7 @@ public interface ResearchDirectionMapper extends BaseMapper<ResearchDirection> {
     @Select("SELECT id,name,father_id,is_leaf FROM research_direction WHERE father_id = #{father_id} AND delete_time IS NULL;")
     List<BriefNode> getSons(@Param("father_id") Long father_id);
 
-    @Select("SELECT id FROM research_direction WHERE root_id = #{root_id} AND path LIKE #{path} AND LEN(delete_time) = 0 AND delete_time IS NULL;")
+    @Select("SELECT id FROM research_direction WHERE root_id = #{root_id} AND path LIKE #{path} AND delete_time IS NULL;")
     List<Long> getAllSons(@Param("root_id") Long root_id, @Param("path") String path);
 
     @Select("SELECT id,name,father_id,is_leaf FROM research_direction;")
